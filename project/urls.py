b64358a387ad66f8519eface787c8542f34f6849
Rupali <rupalsing2828@gmail.com>
2022-12-06 12:46:03 +0530

    fixed

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
 ]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
